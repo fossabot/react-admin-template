9742980335a555e90a6a2470b5ed51404724114a
vanilla <uninge.sun@gmail.com>
2020-12-28 15:51:59 +0800

    feature: 优化

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,25 @@
 module.exports = {
 	plugins: [
+		// 必须在下面那个之前，legacy为true下面那个loose必须为true
 		['@babel/plugin-proposal-decorators', { legacy: true }],
+		// 必须在上面那个之后，上面那个legacy为true，则loose必须为true
 		['@babel/plugin-proposal-class-properties', { loose: true }],
 		['@babel/plugin-transform-runtime', { corejs: { version: 3, proposals: true } }],
-		'syntax-async-functions',
 		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-proposal-export-namespace-from',
+		'@babel/plugin-proposal-optional-chaining',
 		'@babel/plugin-transform-async-to-generator',
 		'@babel/plugin-transform-regenerator',
 		'@babel/plugin-transform-modules-commonjs',
-		'@babel/plugin-syntax-import-meta',
-		'@babel/plugin-proposal-json-strings',
-		'@babel/plugin-proposal-function-sent',
-		'@babel/plugin-proposal-export-namespace-from',
-		'@babel/plugin-proposal-numeric-separator',
-		'@babel/plugin-proposal-throw-expressions',
-		'@babel/plugin-proposal-optional-chaining',
+		// 'syntax-async-functions',
+		// '@babel/plugin-proposal-function-sent',
+		// '@babel/plugin-syntax-import-meta',
+		// '@babel/plugin-proposal-json-strings',
+		// '@babel/plugin-proposal-numeric-separator',
+		// '@babel/plugin-proposal-throw-expressions',
 		'react-hot-loader/babel',
 		[
-			'import',
+			'babel-plugin-import',
 			{
 				libraryName: 'antd',
 				libraryDirectory: 'es',
@@ -31,7 +33,7 @@ module.exports = {
 		[
 			'@babel/preset-env',
 			{
-				useBuiltIns: 'entry',
+				useBuiltIns: 'usage',
 				corejs: {
 					version: 3,
 					proposals: true,
@@ -39,7 +41,7 @@ module.exports = {
 			},
 		],
 		[
-			'react-app',
+			'babel-preset-react-app',
 			{
 				flow: false,
 				typescript: true,
