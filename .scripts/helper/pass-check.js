9ebd00e1d97c0fab866251c9bda29c781618313b
vanilla <uninge.sun@gmail.com>
2021-01-02 16:46:52 +0800

    feature: empty

--- .scripts/helper/pass-check.js
+++ .scripts/helper/pass-check.js
@@ -11,6 +11,7 @@ const envs = ['development', 'test', 'production'];
 const NODE_ENV = process.env.NODE_ENV;
 const BUILD_ENV = process.env.BUILD_ENV;
 
+// logo
 try {
 	console.log(chalk.gray(figlet.textSync(capitalCase(name))));
 } catch (err) {
@@ -26,13 +27,14 @@ if (!semver.satisfies(currentNodeVersion, requiredNodeVersion)) {
 	process.exit(1);
 }
 
-// env
+// BUILD_ENV
 if (!BUILD_ENV) {
 	console.log(chalk.red(' The BUILD_ENV environment variable is required but was not specified.'));
 	console.log();
 	process.exit(1);
 }
 
+// NODE_ENV
 if (!envs.includes(NODE_ENV)) {
 	console.log(chalk.red(` The NODE_ENV environment variable is must be on of [${envs.join(', ')}].`));
 	console.log();
