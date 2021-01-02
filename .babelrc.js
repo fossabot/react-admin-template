e69d31d275b4c7c1151feffaede8e19fe71c72bc
vanilla <uninge.sun@gmail.com>
2021-01-02 11:36:54 +0800

    feature: 环境变量

--- .babelrc.js
+++ .babelrc.js
@@ -9,10 +9,6 @@ module.exports = {
 		'@babel/plugin-transform-async-to-generator', // https://babeljs.io/docs/en/babel-plugin-transform-async-to-generator
 		'@babel/plugin-transform-regenerator', // https://babeljs.io/docs/en/babel-plugin-transform-regenerator
 		'@babel/plugin-transform-modules-commonjs', // https://babeljs.io/docs/en/babel-plugin-transform-modules-commonjs
-		// '@babel/plugin-proposal-function-sent',																								// https://babeljs.io/docs/en/babel-plugin-syntax-function-sent
-		// '@babel/plugin-syntax-import-meta',																										// https://babeljs.io/docs/en/babel-plugin-syntax-import-meta
-		// '@babel/plugin-proposal-json-strings',																									// https://babeljs.io/docs/en/babel-plugin-proposal-json-strings
-		// '@babel/plugin-proposal-throw-expressions',																						// https://babeljs.io/docs/en/babel-plugin-proposal-throw-expressions
 		'react-hot-loader/babel', // https://github.com/gaearon/react-hot-loader#react-hot-loader
 		['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], // https://github.com/ant-design/babel-plugin-import#babel-plugin-import
 	],
