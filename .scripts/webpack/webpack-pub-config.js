b9b0a939df48387ad70c0099d0f7d53df5734462
vanilla <uninge.sun@gmail.com>
2021-01-02 13:23:11 +0800

    feature: 环境变量

--- .scripts/webpack/webpack-pub-config.js
+++ .scripts/webpack/webpack-pub-config.js
@@ -1,7 +1,5 @@
-const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
-// const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 
 const webpackBaseConfig = require('./webpack-base-config');
@@ -17,28 +15,6 @@ const webpackPubConfig = {
 	plugins: [],
 	optimization: {
 		usedExports: true,
-		// splitChunks: {
-		// 	chunks: 'all',
-		// 	minChunks: 1,
-		// 	minSize: 10000,
-		// 	maxAsyncRequests: 5,
-		// 	maxInitialRequests: 3,
-		// 	automaticNameDelimiter: '~',
-		// 	name: true,
-		// 	cacheGroups: {
-		// 		common: {
-		// 			test: /[\\/]node_modules[\\/]/,
-		// 			name: 'vendors',
-		// 			minChunks: 2,
-		// 			priority: 1,
-		// 		},
-		// 		styles: {
-		// 			name: 'styles',
-		// 			test: /\/src\/components\/.*\.less|css$/,
-		// 			priority: 10,
-		// 		},
-		// 	},
-		// },
 		minimizer: [
 			new TerserPlugin({
 				exclude: /\.min\.js$/,
@@ -71,20 +47,6 @@ const webpackPubConfig = {
 			new CssMinimizerWebpackPlugin({
 				sourceMap: canUseSourceMap,
 			}),
-			// new OptimizeCssAssetsPlugin({
-			// 	map: false,
-			// 	assetNameRegExp: /\.css$/g,
-			// 	cssProcessor: require('cssnano'),
-			// 	cssProcessorOptions: {
-			// 		safe: true,
-			// 		autoprefixer: { disable: false },
-			// 		mergeLonghand: false,
-			// 		discardComments: {
-			// 			removeAll: true,
-			// 		},
-			// 	},
-			// 	canPrint: true,
-			// }),
 		],
 		splitChunks: {
 			chunks: 'all',
