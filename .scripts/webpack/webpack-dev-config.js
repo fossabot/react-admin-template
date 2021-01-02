fb8e9a4009240708c44e9a835a25ab16af36b036
vanilla <uninge.sun@gmail.com>
2021-01-02 12:21:41 +0800

    feature: 环境变量

--- .scripts/webpack/webpack-dev-config.js
+++ .scripts/webpack/webpack-dev-config.js
@@ -15,6 +15,11 @@ const webpackDevConfig = {
 		filename: '[name].js',
 		publicPath: '/',
 	},
+	resolve: {
+		alias: {
+			'react-dom': '@hot-loader/react-dom',
+		}
+	},
 	plugins: [
 		new webpack.NamedModulesPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
