7463af93cc83921734698a86a7f845fbc6d706ab
vanilla <uninge.sun@gmail.com>
2021-01-07 18:13:05 +0800

    feature: 优化

--- .scripts/webpack/webpack-dev-config.js
+++ .scripts/webpack/webpack-dev-config.js
@@ -8,6 +8,10 @@ const { dllConfig } = require('../config');
 const paths = require('../config/paths');
 const webpackBaseConfig = require('./webpack-base-config');
 
+const dllScriptExists = fs.existsSync(path.resolve(paths.appDllPath, dllConfig.filename));
+const dllJsonExists = fs.existsSync(path.resolve(paths.appDllPath, dllConfig.manifest));
+const canUseDll = dllScriptExists && dllJsonExists;
+
 const webpackDevConfig = {
 	mode: 'development',
 	devtool: 'cheap-module-source-map',
@@ -21,26 +25,21 @@ const webpackDevConfig = {
 	// 	},
 	// },
 	plugins: [
-		new webpack.NamedModulesPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoEmitOnErrorsPlugin(),
-	],
-};
-
-if (
-	fs.existsSync(path.resolve(paths.appDllPath, dllConfig.filename)) &&
-	fs.existsSync(path.resolve(paths.appDllPath, dllConfig.manifest))
-) {
-	webpackDevConfig.plugins.push(
-		new webpack.DllReferencePlugin({
+		canUseDll && new webpack.DllReferencePlugin({
 			context: __dirname,
 			manifest: require(path.resolve(paths.appDllPath, dllConfig.manifest)),
 		}),
-		new AddAssetHtmlWebpackPlugin({
+		canUseDll && new AddAssetHtmlWebpackPlugin({
 			filepath: require.resolve(path.resolve(paths.appDllPath, dllConfig.filename)),
 		}),
-	);
-}
+	].filter(Boolean),
+	optimization: {
+		namedModules: true,
+		noEmitOnErrors: true,
+	},
+};
+
 
 const config = webpackMerge(webpackBaseConfig, webpackDevConfig);
 
