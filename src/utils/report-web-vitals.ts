d19c5fd1eb93549ac44881aa567809a8a094adbd
vanilla <uninge.sun@gmail.com>
2021-01-09 21:33:59 +0800

    feature: empty

--- src/utils/report-web-vitals.ts
+++ src/utils/report-web-vitals.ts
@@ -1,6 +1,7 @@
 const reportWebVitals = (onPerfEntry?: () => void): void => {
 	if (onPerfEntry) {
-		import('web-vitals').then(({ getCLS, getFID, getLCP, getFCP, getTTFB }) => {
+		// eslint-disable-next-line no-void
+		void import('web-vitals').then(({ getCLS, getFID, getLCP, getFCP, getTTFB }) => {
 			getCLS(onPerfEntry); // Cumulative Layout Shift (CLS)
 			getFID(onPerfEntry); // First Input Delay (FID)
 			getLCP(onPerfEntry); // Largest Contentful Paint (LCP)
