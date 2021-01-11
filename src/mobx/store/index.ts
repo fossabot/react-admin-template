88baeb58748ad8fa9147f85e4f09777842a8220f
sunjingke3 <sunjingke3@jd.com>
2021-01-11 20:56:05 +0800

    feature: empty

--- src/mobx/store/index.ts
+++ src/mobx/store/index.ts
@@ -1,8 +1,8 @@
 /**
  * mobx store入口
- * store文件使用 module.exports 导出，下面代码使用 context(key) 使用
- * store文件使用 export default 导出，但是下面代码需要处理取 context(key).default 属性
- * 注：目前约定统一使用 module.exports 导出，根据喜好自行更改
+ * store文件使用 module.exports 导出，下面代码使用 context(key) 属性
+ * store文件使用 export default 导出，下面代码使用 context(key).default 属性
+ * 注：建议使用一种导出模式，方便以后全局变更修改适配，根据喜好自行选择
  */
 
 const store: { [key: string]: any } = {};
@@ -13,20 +13,23 @@ const keys = context.keys().filter((item: string) => item !== './index.ts');
 
 keys.forEach((key: string) => {
 	const pathKeys = key.replace(/\.\//, '').replace(/\.ts$/g, '').split('/');
+	// eslint-disable-next-line max-len
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access,no-underscore-dangle
+	const module = context(key).__esModule ? context(key).default : context(key);
 
 	if (pathKeys.length === 1) {
 		if (pathKeys[0] === 'pages') {
 			throw new Error('pages已被文件夹占用，请更换文件名');
 		}
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		store[pathKeys[0]] = context(key);
+		store[pathKeys[0]] = module;
 	}
 
 	// 页面级store全部挂载在pages对象下
 	// 不过不建议页面级store写在此文件夹下，建议写在具体页面所在目录
 	if (pathKeys.length === 2) {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-		store.pages[pathKeys[1]] = context(key);
+		store.pages[pathKeys[1]] = module;
 	}
 
 	if (pathKeys.length > 2) {
