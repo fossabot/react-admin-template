8f1e5a88b53d959e0dc8c86ba63a171ffda1fc00
sunjingke3 <sunjingke3@jd.com>
2021-01-13 19:58:04 +0800

    feature: 消息中心

--- src/app.tsx
+++ src/app.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
 	return (
 		<BrowserRouter>
 			<ConfigProvider locale={zhCN}>
-				{/* @warn @todo 删除。此处仅是模板示例，真实项目请选择其一。Attention: One of MobxProvider and ReduxProvider */}
+				{/* @warn @todo 删除。此处仅是模板示例，真实项目请选择其一。⚠CAUTION: One of MobxProvider and ReduxProvider */}
 				<MobxProvider>
 					<ReduxProvider>
 						<Switch>
