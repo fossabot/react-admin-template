875da7734e487d6484de2c52a614d150ed063403
vanilla <uninge.sun@gmail.com>
2021-01-14 23:40:04 +0800

    feature: 优化

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 
-import BaseLayout from './layout/base-layout';
-import { getRedirectsRoutes, renderRoutes } from './utils/render-routes';
-import outsiders from './router/outsiders';
-import './styles/index.less';
+import BaseLayout from '@/layout/base-layout';
+import { getRedirectsRoutes, renderRoutes } from '@/utils/render-routes';
+import outsiders from '@/router/outsiders';
+import '@/styles/index.less';
 
 import MobxProvider from './mobx';
 import ReduxProvider from './redux';
