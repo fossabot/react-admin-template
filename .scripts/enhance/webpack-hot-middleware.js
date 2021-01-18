212f787d59118b55327eea2a95679cf0687680b6
sunjingke3 <sunjingke3@jd.com>
2021-01-18 15:37:46 +0800

    feature: log

--- .scripts/enhance/webpack-hot-middleware.js
+++ .scripts/enhance/webpack-hot-middleware.js
@@ -9,7 +9,16 @@ hotClient.subscribe((event) => {
 	 * https://github.com/jantimon/html-webpack-plugin/issues/680
 	 */
 	if (event.action === 'reload') {
-	  window.location.reload()
+		// window.location.reload()
+
+		const dom = document.querySelector('#webpack-hot-middleware-notice');
+		if (dom) {
+			dom.textContent = 'Compile Success!';
+
+			setTimeout(() => {
+				dom.offsetParent.removeChild(dom);
+			}, 350);
+		}
 	}
 
 	/**
@@ -17,23 +26,20 @@ hotClient.subscribe((event) => {
 	 * giving notice for an expected reload of the `electron` process
 	 */
 	if (event.action === 'compiling') {
+		const style = `
+			position: absolute;
+			bottom: 20px;
+			left: 20px;
+			padding: 8px 12px;
+			color: #f90;
+			font-size: 16px;
+			font-weight: 700;
+			background: #000;
+			border-radius: 4px;
+			box-shadow: 0 4px 5px 0 #f90, 0 1px 10px 0 #f90;
+		`;
 		document.body.innerHTML += `
-      <style>
-        #dev-client {
-          background: #4fc08d;
-          border-radius: 4px;
-          bottom: 20px;
-          box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.3);
-          color: #fff;
-          font-family: 'Source Sans Pro', sans-serif;
-          left: 20px;
-          padding: 8px 12px;
-          position: absolute;
-        }
-      </style>
-      <div id="dev-client">
-        Compiling Main Process...
-      </div>
+      <div id="webpack-hot-middleware-notice" style="${style}">Main Process Compiling...</div>
     `;
 	}
 });
