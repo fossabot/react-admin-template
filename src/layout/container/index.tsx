9d1b8bcc8ac754b0d0fa7c967541d0abf66fe5c7
vanilla <uninge.sun@gmail.com>
2021-01-23 22:22:10 +0800

    feat: 边缘逻辑处理

--- src/layout/container/index.tsx
+++ src/layout/container/index.tsx
@@ -1,14 +1,60 @@
 /**
- * 本来打算用此组件处理一些全局逻辑
- * 经实践发现大多为一些边缘逻辑，为减少一层嵌套因此挪到`src/pages/reactive`路由页面处理了
+ * 处理一些全局逻辑
  */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router';
+import { flatRoutes } from '@/router';
 
 interface IProps {
 	children: React.ReactElement;
 }
 
 const Container: React.FC<IProps> = (props) => {
+	const history = useHistory();
+	const location = useLocation();
+
+	useEffect(() => {
+		const title = document.title;
+		setTitle(location.pathname, title);
+
+		const unListen = history.listen(({ pathname }) => {
+			setTitle(pathname, title);
+		});
+
+		return () => {
+			unListen();
+			document.title = title;
+		};
+	}, []);
+
+	function setTitle(pathname: string, originTitle: string) {
+		getTitle(pathname)
+			.then((title) => {
+				if (typeof title === 'string') {
+					document.title = title;
+				}
+			})
+			.catch(() => {
+				document.title = originTitle;
+			});
+	}
+
+	function getTitle(pathname: string) {
+		return new Promise((resolve, reject) => {
+			const match = flatRoutes.find((route) => {
+				const path = route.path;
+
+				return path.replace(/\/$/, '') === pathname.replace(/\/$/, '');
+			});
+
+			if (match && match.meta?.title) {
+				resolve(match.meta.title);
+			} else {
+				reject();
+			}
+		});
+	}
+
 	return props.children;
 };
 
