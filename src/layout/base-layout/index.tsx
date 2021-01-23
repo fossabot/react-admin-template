d2b8f8276369566d65fd2a42e0157eded629ec9b
vanilla <uninge.sun@gmail.com>
2021-01-23 22:02:43 +0800

    feat: 全局组件

--- src/layout/base-layout/index.tsx
+++ src/layout/base-layout/index.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import { Switch } from 'react-router';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation, useHistory, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import { MenuFoldOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
 
+import Reactive from '@/pages/reactive';
 import { RootState } from '@/redux/store';
 import { getRedirectsRoutes, renderRoutesDeep } from '@/utils/render-routes';
 import routes from '@/router';
+
 import GlobalMenu from './components/global-menu';
 import GlobalBreadcrumb from './components/global-breadcrumb';
 import HeaderRight from './components/header-right';
+import TabsBar from './components/tabs-bar';
 import s from './index.module.less';
 
 const { Header, Sider, Content } = Layout;
@@ -46,11 +49,13 @@ const BaseLayout: React.FC = () => {
 								setCollapsed(!collapsed);
 							}}
 						/>
-						<GlobalBreadcrumb />
+						<TabsBar />
 					</section>
 					<HeaderRight />
 				</Header>
+				<GlobalBreadcrumb />
 				<Content className={s.appRouterView}>
+					<Route component={Reactive} />
 					<Switch>
 						{getRedirectsRoutes(routes)}
 						{renderRoutesDeep(routes, { permissions })}
