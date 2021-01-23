c3a2161519124dfb71b387dbc3e343abb4dd3caa
vanilla <uninge.sun@gmail.com>
2021-01-23 23:25:48 +0800

    feat: 面包屑

--- src/layout/base-layout/components/global-breadcrumb/index.tsx
+++ src/layout/base-layout/components/global-breadcrumb/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Breadcrumb } from 'antd';
+import s from './index.module.less';
 
 const GlobalBreadcrumb: React.FC = () => {
 	return (
-		<Breadcrumb>
+		<Breadcrumb className={s.breadcrumb}>
 			<Breadcrumb.Item>Home</Breadcrumb.Item>
 			<Breadcrumb.Item>Application Center</Breadcrumb.Item>
 			<Breadcrumb.Item>Application List</Breadcrumb.Item>
