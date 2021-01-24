53d1c479c426adb2fed066a4a4a8125dfa447628
vanilla <uninge.sun@gmail.com>
2021-01-24 22:07:44 +0800

    feat: 刷新页面

--- src/pages/refresh/index.tsx
+++ src/pages/refresh/index.tsx
@@ -1,7 +1,37 @@
+/**
+ * 应用内刷新中转页面
+ * @description 思路: 原路由跳转至本中转页面再跳转回去以完整刷新
+ */
 import React from 'react';
+import { useLocation, useHistory } from 'react-router';
+import useEnhancedEffect from '@/utils/use-enhanced-effect';
+
+interface IState {
+	pathname: string;
+	search?: string;
+	hash?: string;
+	originState: any;
+}
 
 const Refresh: React.FC = () => {
-	return <div>Refresh</div>;
+	const location = useLocation<IState>();
+	const history = useHistory();
+	const state = location.state;
+
+	useEnhancedEffect(() => {
+		if (!state) {
+			history.replace('/');
+		} else {
+			history.replace({
+				pathname: state.pathname,
+				search: state.search,
+				hash: state.hash,
+				state: state.originState,
+			});
+		}
+	}, []);
+
+	return null;
 };
 
 export default Refresh;
