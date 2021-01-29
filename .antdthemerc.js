2cc7dd423867be7d17de624fed43625b05885c49
vanilla <uninge.sun@gmail.com>
2021-01-30 00:11:59 +0800

    feat: 主题

--- .antdthemerc.js
+++ .antdthemerc.js
@@ -1,15 +1,19 @@
 const { getThemeVariables } = require('antd/dist/theme');
 
 // https://ant.design/docs/react/customize-theme-cn
-// 如果启用此项或者使用了babel-plugin-import，需确保less-loader版本小于等于6.2.0
+// 如果启用此项`babel-plugin-import`需要设置`style: true`以使用`less`，同时需需要`less-loader`版本小于等于`6.2.0`
 module.exports = {
 	enable: true,
 	antdThemeVars: {
 		...getThemeVariables({
-			dark: true,
+			dark: false,
 			compact: false,
 		}),
 		// 其他变量，eg.
 		// 'primary-color': '#f90',
+		'layout-header-background': '#1f1f1f',
+		'menu-dark-bg': '#1f1f1f',
+		'menu-popup-bg': '#1f1f1f',
+		'menu-dark-inline-submenu-bg': '#141414',
 	},
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Card } from 'antd';
 
 const Home: React.FC = () => {
-	return <div>本分支代码为极简开发模板，除了优化不会提交过多的代码</div>;
+	return <Card>本分支代码为极简开发模板，除了优化不会提交过多的代码</Card>;
 };
 
 export default Home;
