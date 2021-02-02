1b0bf3c653e3a0ed1dbe207ef3d05577f14d3e99
uninge <uninge.sun@gmail.com>
2021-02-02 10:57:32 +0800

    feat: packager

--- .scripts/web/build.js
+++ .scripts/web/build.js
@@ -34,7 +34,7 @@ rimraf(appWebDistPath, (err) => {
 			console.log(`${res}`);
 			console.log(chalk.green('------------------ 构建日志输出结束 ------------------'));
 			console.log();
-			console.log(` ${chalk.bold(chalk.green('✔'))} ${chalk.cyan(`构建完成！可在${chalk.green(`【${appWebDistPath}】`)}目录查看或进行下一步操作`)}`);
+			console.log(` ${chalk.bold.green('✔')} ${chalk.cyan(`构建完成！可在${chalk.green(`【${appWebDistPath}】`)}目录查看或进行下一步操作`)}`);
 			console.log();
 		})
 		.catch((err) => {
